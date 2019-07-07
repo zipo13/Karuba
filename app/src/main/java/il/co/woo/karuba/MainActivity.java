cfef26f600a18ca7315a29ecdf4e6f209857e8ec
Itamar <zipo13@gmail.com>
2019-07-08 00:00:04 +0300

    Add ViewModel to separate data from UI

--- app/src/main/java/il/co/woo/karuba/MainActivity.java
+++ app/src/main/java/il/co/woo/karuba/MainActivity.java
@@ -1,27 +1,32 @@
 package il.co.woo.karuba;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import java.util.Random;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int NUMBER_OF_TILES = 36;
+
+    private static final int NUMBER_OF_TILE_ROWS = 6;
+    private static final int NUMBER_OF_TILE_COLUMNS = 6;
+
     private static final String TILE_IMG_NAME_PREFIX = "tile_";
     private static final String DRAWABLE_TYPE = "drawable";
     private static final String TAG = "MainActivity";
+    private TilesViewModel mViewModel;
+
+
 
     @BindView(R.id.new_tile) ImageButton mNewTileButton;
     @BindView(R.id.last_tile) ImageView mLastTileImageView;
+    @BindView(R.id.game_board) ImageView mGameBoardImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mViewModel = ViewModelProviders.of(this).get(TilesViewModel.class);
+
+
 
         mNewTileButton.setOnClickListener(view -> {
             Log.d(TAG, "onClick: User clicked on new tile button");
@@ -38,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void drawNewTile() {
         Log.d(TAG, "drawNewTile: Enter");
-        //generate a random number between 1-36
-        final int random = new Random().nextInt(NUMBER_OF_TILES) + 1;
+        int random = mViewModel.getRandomTile();
+        if (random == 0)
+            return;
 
         //replace the image in the last tile image view with the tile number generated
 
@@ -53,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.d(TAG, "drawNewTile: failed to located tile resource id for tile name: " + tileFileName);
         }
-
-
     }
+
 }
---
+++ app/src/main/java/il/co/woo/karuba/TilesViewModel.java
@@ -0,0 +1,43 @@
+package il.co.woo.karuba;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TilesViewModel extends ViewModel {
+    private static final int NUMBER_OF_TILES = 36;
+
+    private ArrayList<Integer> mTileArray;
+    private int mNumberOfSelectedTiles;
+
+    public TilesViewModel() {
+        initTiles();
+    }
+
+    public void initTiles() {
+        //no tiles selected yet
+        mNumberOfSelectedTiles = 0;
+        if (mTileArray != null)
+            mTileArray.clear();
+
+        //create the tile array to simulate the bag and populate it with tile numbers
+        mTileArray = new ArrayList<>();
+        for (int i = 1; i <= NUMBER_OF_TILES; i++ ) {
+            mTileArray.add(i);
+        }
+    }
+
+
+    int getRandomTile() {
+        if (mNumberOfSelectedTiles >= NUMBER_OF_TILES)
+            return 0;
+        //generate a random number between 1-36
+        final int random = new Random().nextInt(NUMBER_OF_TILES-mNumberOfSelectedTiles);
+        //increase the number of selected tiles;
+        mNumberOfSelectedTiles++;
+        int randomTileNumber = mTileArray.get(random);
+        mTileArray.remove(random);
+        return randomTileNumber;
+    }
+}
